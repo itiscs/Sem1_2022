5870b122b7f73544a6cdc07f5a9ecaeaf0cbce7c
itiscs <moudroff@gmail.com>
2022-09-20 14:37:03 +0300

    x^10 + 2x^9 + 3x^8 + ... + 10x + 11

--- Test4Git/Test4Git/Program.cs
+++ Test4Git/Test4Git/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello from github!");
-Console.WriteLine("Add new line!");
+Console.WriteLine("Input x:");
+
+double x = double.Parse(Console.ReadLine());
+
+double y = 0;
+double p = 1;
+int i = 10;
+
+while(i > 0)
+{
+    y = y + i * p;
+    p = p * x;
+    i--;
+}
+
+Console.WriteLine(y);
