b95bc77a43d895d2805f33f8f62e6caa519e7c91
itiscs <moudroff@gmail.com>
2022-09-27 14:06:13 +0300

    Фибоначчи

---
+++ FibApp/FibApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input k:");
+int k = int.Parse(Console.ReadLine());
+
+long x1, x2, x = 0;
+x1 = 1;
+x2 = 1;
+for(int i = 3; i <= k; i++)
+{
+    x = x1 + x2;
+    x2 = x1;
+    x1 = x;
+    Console.WriteLine($"{i} --- {x}");
+
+}
+//Console.WriteLine(x);
