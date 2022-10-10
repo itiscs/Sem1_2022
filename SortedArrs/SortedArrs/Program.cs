cf64393e07c3beffc283dad2877afd1832a2e224
itiscs <moudroff@gmail.com>
2022-10-10 15:07:40 +0300

    Упорядоченные массивы

---
+++ SortedArrs/SortedArrs/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+int[] a = new int[] { -21, 16, 17, 22, 23, 24};
+int[] b = new int[] { 6, 7, 18, 8, 10, 12};
+
+var na = a.Length;
+var nb = b.Length;
+int[] c = new int[na + nb];
+int i =0 , j = 0, k = 0;
+
+while(i < na && j < nb)
+{
+    if (a[i] <= b[j])
+        c[k++] = a[i++];
+    else
+        c[k++] = b[j++];        
+}
+if (i == na) // закончился массив a
+    for (i = j; i < nb; i++)
+        c[k++] = b[i];
+else         // закончился массив b
+    for (j = i; j < na; j++)
+        c[k++] = a[j];
+
+
+for(i = 0; i< c.Length; i++)
+    Console.WriteLine(c[i]);
+
+
+
+
+
