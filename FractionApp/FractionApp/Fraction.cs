e38a36a09eba85bb58ae94626b59f726a576ce74
itiscs <moudroff@gmail.com>
2022-11-14 17:16:01 +0300

    Fraction

---
+++ FractionApp/FractionApp/Fraction.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionApp
+{
+    public class Fraction
+    {
+        private int numerator;
+        private int denominator;
+
+        public Fraction(int n, int d)
+        {
+            if (d == 0)
+                throw new ArgumentException("Нельзя делить на ноль!");
+
+            numerator = n;
+            denominator = d;
+        }
+
+        public Fraction(int n):this(n, 1)
+        {
+            //numerator = n;
+            //denominator = 1;
+        }
+        public Fraction():this(0, 1)
+        {
+            //numerator = 0;
+            //denominator = 1;
+        }
+
+        private void PrivMethod()
+        {
+
+
+        }
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("Static method");
+        }
+        public void SetNum(int n) 
+        {
+            numerator = n;  
+        }
+
+        public int GetNum()
+        {
+            return numerator;
+        }
+
+        public void SetDenom(int d)
+        {
+            if (d == 0)
+                throw new ArgumentException("Нельзя делить на ноль!");
+            denominator = d;
+        }
+
+        public int GetDen()
+        {
+            return denominator;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{numerator}/{denominator}");
+        }
+
+
+        public void Sokr()
+        {
+            //ToDo
+        }
+
+
+        public void Add(Fraction f)
+        {
+            this.numerator = this.numerator * f.denominator +
+                this.denominator * f.numerator;
+            this.denominator = this.denominator * f.denominator;
+            Sokr();
+        }
+
+        public void Add(Fraction f1, Fraction f2)
+        {
+            this.numerator = f1.numerator * f2.denominator +
+                f1.denominator * f2.numerator;
+            this.denominator = f1.denominator * f2.denominator;
+            Sokr();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ FractionApp/FractionApp/Program.cs
@@ -0,0 +1,50 @@
+﻿
+namespace FractionApp
+{
+    
+    public class Program
+    {
+        public static void Main()
+        {
+            //try
+            //{
+            Fraction f1 = new Fraction(1,2);
+            Fraction f2 = new Fraction(32);
+            Fraction f3 = new Fraction();
+
+            f1.SetNum(3);
+            //f1.SetDenom(2);
+
+            Fraction.StaticMethod();
+            
+            
+            f1.Print();
+             
+            //f2.SetNum(2);
+            f2.SetDenom(1);
+
+            Console.WriteLine($"Fraction - {f2.GetNum()}/{f2.GetDen()}");
+
+            f3.Print();
+
+
+
+            f1.Add(f2);
+            f1.Print();
+
+            f3.Add(f3, f2);
+            //f3 = f3 + f2;
+            f3.Print();
+
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+        }
+
+    }
+
+}
+
